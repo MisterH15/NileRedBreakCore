182ca8db7a76d4e631ff6845dae36fc855fa7ec1
MisterH15 <lemoneysnicket09@gmail.com>
2024-04-10 17:16:13 -0500

    Create sauce.js

---
+++ sauce.js
@@ -0,0 +1,38 @@
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+var audioplayer;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '1080',
+        width: '1920',
+        videoId: 'IrdYueB9pY4',
+        events: {
+            'onReady': onPlayerReady
+        }
+    });
+  audioplayer = new YT.Player('audioplayer', {
+        height: '1',
+        width: '1',
+        videoId: 'RgFaK6ZQifE',
+        events: {
+            'onReady': onAudioPlayerReady
+        }
+    });
+}
+
+function onPlayerReady(event) {
+    player.setPlaybackRate(2);
+    player.mute();
+    event.target.playVideo();
+    player.seekTo(90);
+}
+
+function onAudioPlayerReady(event) {
+    event.target.playVideo();
+    audioplayer.seekTo(50);
+}
